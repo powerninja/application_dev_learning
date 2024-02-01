b39dbc9a4310b1394c4b0fd6b4e416d01995584b
powerninja <asdyuy23@gmail.com>
2024-02-02 00:35:30 +0900

    Container追加

--- lib/main.dart
+++ lib/main.dart
@@ -49,21 +49,25 @@ class _MyHomePageState extends State<MyHomePage> {
         actions: [Icon(Icons.add), Icon(Icons.share)],
       ),
       body: Center(
-          child: Padding(
-        padding: const EdgeInsets.all(32),
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      )),
+          child: Container(
+              color: Colors.amberAccent,
+              height: double.infinity,
+              width: 600,
+              child: Padding(
+                padding: const EdgeInsets.all(32),
+                child: Column(
+                  mainAxisAlignment: MainAxisAlignment.center,
+                  children: <Widget>[
+                    const Text(
+                      'You have pushed the button this many times:',
+                    ),
+                    Text(
+                      '$_counter',
+                      style: Theme.of(context).textTheme.headlineMedium,
+                    ),
+                  ],
+                ),
+              ))),
       floatingActionButton: FloatingActionButton(
         onPressed: _incrementCounter,
         tooltip: 'Increment',

6bb7cd9a777fa8d2ee7f7bc815b2a9df18ff8131
powerninja <asdyuy23@gmail.com>
2024-02-03 23:35:27 +0900

    画面遷移用のページ追加

---
+++ lib/next_page.dart
@@ -0,0 +1,14 @@
+import 'package:flutter/material.dart';
+
+class nextPage extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Scaffold(
+        appBar: AppBar(
+          title: Text("Hello Flutter"),
+        ),
+        body: Container(
+          color: Colors.red,
+        ));
+  }
+}
